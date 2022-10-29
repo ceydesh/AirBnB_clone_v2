fe52328f44e1739e5359feb98ea7017935781d72
Derrick Henson <84587372+ceydesh@users.noreply.github.com>
2022-10-29 06:53:00 +0300

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
-#script that sets up your web servers for the deployment of web_static
+# Script that configures Nginx server with some folders and files
 apt-get -y update
 apt-get -y install nginx
-ufw allow 'Nginx HTTP'
-mkdir -p /data/web_static/
+service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    <h1>Welcome to System Engineering and Devops Bostone</h1>
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
